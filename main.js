4d83fa08551c703793e997f5437c14c8935da595
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-27 22:27:45 -0300

    refact: editing diferenca

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ for(let i = 1; i <= 5; i++ ){
             alert("Errou, faltam: " + diferenca + " a menos");
             alert("Número de tentativas: " + i);    
         }else{
-            alert ("Errou, faltam: "+ diferenca +" mais") ;
+            alert ("Errou, faltam: "+ diferenca +" a mais") ;
         }
     }
 } 
